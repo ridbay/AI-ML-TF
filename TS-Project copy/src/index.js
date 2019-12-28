06a7a8a9c8f2080645bbf45bc21f04a835d3bb01
Ridwan Balogun <balogunridwan@gmail.com>
2019-12-28 14:33:54 +0100

    Add NSFWJS project

--- TS-Project copy/src/index.js
+++ TS-Project copy/src/index.js
@@ -1,7 +1,28 @@
 import "./styles.css";
-import * as tf from '@tensorflow/tfjs'
+import * as nsfwjs from "nsfwjs";
 
-document.getElementById("app").innerHTML = `
-<h1>Ridwan TensorFlow</h1>
-${tf.version.tfjs}
-`;
+const img = document.getElementById("img");
+
+// Load model from my S3.
+// See the section hosting the model files on your site.
+nsfwjs.load().then(function(model) {
+  model.classify(img).then(function(predictions) {
+    // Classify the image
+    console.log("Predictions: ", predictions);
+    document.getElementById("app").innerText = `
+    <b>Identified as ${predictions[0].className}</b>
+    ${predictions[0].className} - ${Math.round(
+      predictions[0].probability * 100
+    )}%
+    ${predictions[1].className} - ${Math.round(
+      predictions[1].probability * 100
+    )}%
+    ${predictions[2].className} - ${Math.round(
+      predictions[2].probability * 100
+    )}%
+    ${predictions[3].className} - ${Math.round(
+      predictions[3].probability * 100
+    )}%
+    `;
+  });
+});
